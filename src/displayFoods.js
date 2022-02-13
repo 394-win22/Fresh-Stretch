0acd76ea42320300cbbeb88051db62bc2051f119
kelly00su <56286184+kelly00su@users.noreply.github.com>
2022-02-13 17:08:03 -0600

    display food start

---
+++ src/displayFoods.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Card, Col, Container, Row, Dropdown, DropdownButton } from "react-bootstrap";
+
+
+const DisplayFoods = () => {
+
+    return (
+        <div>
+            <Row className="g-4">
+                banana
+            </Row>
+            <Row className="g-4">
+                banana
+            </Row>
+            <Row className="g-4">
+                banana
+            </Row>
+        </div>
+    );
+
+}
+
+
+
+
+
+
+export default DisplayFoods
