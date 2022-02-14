987c4e6e3aecf08ea9a453409d95cc62d49eb9e5
s. park <parkteddy2845@gmail.com>
2022-02-13 18:12:22 -0600

    add header & footer

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import React from "react";
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useData, getItemsFromUser } from "./firebase";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import DisplayFoods from "./displayFoods";
+import FoodsPage from "./pages/FoodsPage";
 import UserPage from "./pages/UserPage";
 
 function App() {
-	const [data, loading, error] = useData("/");
-
-	if (error) return <h1>{error}</h1>;
-	if (loading) return <h1>Loading the schedule...</h1>;
-
-	console.log(data);
-
 	return (
 		<div>
 			<BrowserRouter>
 				<Header />
 				<Routes>
-					<Route path="/" element={<DisplayFoods />} />
+					<Route path="/" element={<FoodsPage />} />
 					<Route path="/user" element={<UserPage />} />
 				</Routes>
 				<Footer />
