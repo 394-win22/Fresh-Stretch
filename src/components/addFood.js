82ad6922811123525d562ca0a9e6de005893ac5d
s. park <parkteddy2845@gmail.com>
2022-03-08 18:23:16 -0600

    other

--- src/components/addFood.js
+++ src/components/addFood.js
@@ -1,166 +1,132 @@
 import React from "react";
-import { Button, Container, Row} from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 
-import { Dialog, DialogContent, DialogTitle, DialogActions } from "@mui/material";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Checkbox from "@mui/material/Checkbox";
-import Avatar from "@mui/material/Avatar";
-import Form from 'react-bootstrap/Form'
+import {
+	Checkbox,
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	DialogActions,
+	FormControlLabel,
+	FormGroup,
+	TextField,
+} from "@mui/material";
 
 import { PlusSquareFill } from "react-bootstrap-icons";
 
 import { useData, pushData } from "../utils/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-function CheckboxListSecondary({ foodInfo, checked, setChecked }) {
+const Checklist = ({ foodInfo, checked, setOtherName }) => {
 	foodInfo = Object.entries(foodInfo);
 
 	const handleToggle = (value) => () => {
-		
-		const currentIndex = checked.indexOf(value);
-		const newChecked = [...checked];
-
-		if (currentIndex === -1) {
-			newChecked.push(value);
-			console.log("index", checked)
+		if (!checked.includes(value)) {
+			checked.push(value);
 		} else {
-			newChecked.splice(currentIndex, 1);
-		}
-		setChecked(newChecked);
-		if (currentIndex == numFoods+1){
-			console.log("other is checked")
+			checked.splice(value, 1);
 		}
 	};
-	// const handleOtherToggle = (value) => () => {
-	// 	const currentIndex = checked.indexOf(value);
-	// 	const newChecked = [...checked];
 
-	// 	if (currentIndex === -1) {
-	// 		newChecked.push(value);
-	// 	} else {
-	// 		newChecked.splice(currentIndex, 1);
-	// 	}
-	// 	setChecked(newChecked);
-	// 	setOtherChecked(checked.indexOf(value));
-	// }
-	let numFoods = 0
+	let numFoods = 0;
 
-	console.log("CHECKED", checked);
 	return (
-		<List
-			dense
-			sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-		>
+		<FormGroup>
 			{Object.entries(foodInfo).map((value) => {
-				numFoods += 1
+				numFoods += 1;
 				const labelId = `checkbox-list-secondary-label-${value}`;
 				return (
-					<ListItem
+					<FormControlLabel
 						key={value[0]}
-						secondaryAction={
-							<Checkbox
-								edge="end"
-								onChange={handleToggle(value[0])}
-								checked={checked.indexOf(value[0]) !== -1}
-								inputProps={{ "aria-labelledby": labelId }}
-							/>
-						}
-						disablePadding
-					>
-						<ListItemButton>
-							<ListItemAvatar>
-								<Avatar>{value[1][0][0]}</Avatar>
-							</ListItemAvatar>
-							<ListItemText
-								id={labelId}
-								primary={`${value[1][0]}`}
-							/>
-						</ListItemButton>
-					</ListItem>
+						id={labelId}
+						control={<Checkbox />}
+						label={value[1][0]}
+						labelPlacement="start"
+						onChange={handleToggle(value[0])}
+					/>
 				);
 			})}
-			<ListItem
-						key={numFoods + 1}
-						secondaryAction={
-							<Checkbox
-								edge="end"
-								onChange={handleToggle((numFoods+1).toString())}
-
-								checked={checked.indexOf(numFoods+1) !== -1}
-								inputProps={{ "aria-labelledby": `checkbox-list-secondary-label-${numFoods+1}` }}
-							/>
-						}
-						disablePadding
-					>
-						<ListItemButton>
-							<ListItemAvatar>
-								<Avatar>O</Avatar>
-							</ListItemAvatar>
-							<ListItemText
-								id={`checkbox-list-secondary-label-${numFoods+1}`}
-								primary="Other"
-							/>
-						</ListItemButton>
-			</ListItem>
-		</List>
+			<FormControlLabel
+				key={numFoods + 1}
+				id={`checkbox-list-secondary-label-${numFoods + 1}`}
+				control={<Checkbox />}
+				label={
+					<TextField
+						placeholder="Other"
+						variant="standard"
+						size="small"
+						onBlur={(ev) => {
+							setOtherName(ev.target.value);
+						}}
+					/>
+				}
+				labelPlacement="start"
+				onChange={handleToggle(numFoods + 1)}
+			/>
+		</FormGroup>
 	);
-}
+};
 
-const saveFood = async (foodInfo, checked, uid, StorageLocation) => {
+const SaveFood = async (foodInfo, checked, otherName, uid, StorageLocation) => {
+	console.log(foodInfo);
+
 	for (var i in checked) {
 		var today = new Date();
 		var index = parseInt(checked[i]);
+
+		console.log(index);
+
 		try {
 			await pushData(`/${StorageLocation}/${uid}`, {
-				Name: foodInfo[index][0],
+				Name:
+					index === foodInfo.length + 1
+						? otherName
+						: foodInfo[index][0],
 				TimeAdded: today.getTime(),
 			});
 		} catch (error) {
 			console.log(error);
 		}
-		console.log("Food Saved");
 	}
 };
 
-const AddFood = ({StorageLocation}) => {
+const AddFood = ({ StorageLocation }) => {
 	const auth = getAuth();
 	let [uid, setUID] = useState(null);
 	let navigate = useNavigate();
 	useEffect(() => {
-		let authToken = sessionStorage.getItem('Auth Token')
+		let authToken = sessionStorage.getItem("Auth Token");
 
 		if (!authToken) {
-			navigate('/login')
+			navigate("/login");
 		}
-	}, [navigate])
+	}, [navigate]);
 	onAuthStateChanged(auth, (authuser) => {
 		if (authuser) {
-		  	// The user's ID, unique to the Firebase project. Do NOT use
-        	// this value to authenticate with the backend server
-        	// Use User.getToken() instead.
-        	setUID(authuser.uid);
+			setUID(authuser.uid);
 		}
-	  });
-	const [open, setOpen] = React.useState(false);
+	});
 
+	const [open, setOpen] = React.useState(false);
 	const [checked, setChecked] = React.useState([]);
-	//const [otherChecked, setOtherChecked] = React.useState(false);
+	const [otherName, setOtherName] = useState("");
 
 	const handleClickOpen = () => {
 		setOpen(true);
 	};
+
 	const handleClose = () => {
+		setChecked([]);
 		setOpen(false);
+		console.log(otherName);
+		setOtherName("");
 	};
 
-	const [foodInfo, foodInfoLoading, foodInfoError] = useData(`/FoodInfo/${StorageLocation}/`);
+	const [foodInfo, foodInfoLoading, foodInfoError] = useData(
+		`/FoodInfo/${StorageLocation}/`
+	);
 	if (foodInfoError) return <h1>{foodInfoError}</h1>;
 	if (foodInfoLoading) return <h1>Loading list of foods...</h1>;
 
@@ -177,46 +143,32 @@ const AddFood = ({StorageLocation}) => {
 				<DialogContent>
 					<Container>
 						<Row>
-							<CheckboxListSecondary
+							<Checklist
 								foodInfo={foodInfo}
 								checked={checked}
-								setChecked={setChecked}
-								// setOtherChecked={setOtherChecked}
+								setOtherName={setOtherName}
 							/>
 						</Row>
-						
-							{/* <Form>
-								<Form.Group className="mb-3" controlId="formBasicEmail" >
-									<Form.Label>Food Name</Form.Label>
-									<Form.Control type="text" style={{borderColor:"darkGrey"}} />
-									<Form.Text className="text-muted">
-									</Form.Text>
-								</Form.Group>
-
-								<Form.Group className="mb-3" controlId="formBasicPassword">
-									<Form.Label>Expiration Days</Form.Label>
-									<Form.Control type="text" style={{borderColor:"darkGrey"}}/>
-								</Form.Group>
-							</Form> */}
-											
 					</Container>
-					
-					
 				</DialogContent>
 				<DialogActions>
 					<Button
-								onClick={() => {
-									saveFood(
-										Object.entries(foodInfo),
-										checked,
-										uid,
-										StorageLocation
-									);
-									setOpen(false);
-								}}
-								style={{backgroundColor:"#FF914D",borderColor:"#FF914D"}}
-							>
-								Confirm
+						onClick={() => {
+							SaveFood(
+								Object.entries(foodInfo),
+								checked,
+								otherName,
+								uid,
+								StorageLocation
+							);
+							handleClose();
+						}}
+						style={{
+							backgroundColor: "#FF914D",
+							borderColor: "#FF914D",
+						}}
+					>
+						Confirm
 					</Button>
 				</DialogActions>
 			</Dialog>
