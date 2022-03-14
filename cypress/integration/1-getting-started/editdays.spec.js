2e9982d1fedcaaf293e944bfb23d9cd018bd731e
cristinabarclay <57467401+cristinabarclay@users.noreply.github.com>
2022-03-13 21:13:26 -0500

    works

--- cypress/integration/1-getting-started/editdays.spec.js
+++ cypress/integration/1-getting-started/editdays.spec.js
@@ -7,11 +7,24 @@ describe ('Edit expiration days', () => {
         cy.get('[data-cy=email]').type('test@a.com')
         cy.get('[data-cy=password]').type('123456')
         cy.get('[data-cy=signinbutton]').click();
-        const expdays = cy.get('[data-cy=expiration]');
-        cy.get('[data-cy=foodlist]').click();
-        cy.get('[data-cy=minusdays]').click();
-        cy.get('[data-cy=save]').click();
-        expect(cy.get('[data-cy=expiration]')).to.not.equal(expdays)
+        cy.get('[data-cy=expiration]').invoke('text')
+        .then((text1) => {
+            cy.get('[data-cy=foodlist]').click();
+            cy.get('[data-cy=minusdays]').click();
+            cy.get('[data-cy=save]').click();
+
+            // grab the div again and compare its previous text
+            // to the current text
+            cy.get('[data-cy=expiration]')
+                .invoke('text')
+                .should((text2) => {
+                    expect(text1).not.to.eq(text2)
+            })
+        })
+        // cy.get('[data-cy=foodlist]').click();
+        // cy.get('[data-cy=minusdays]').click();
+        // cy.get('[data-cy=save]').click();
+        //expect(cy.get('[data-cy=expiration]')).to.not.equal(expdays)
   
       });
   
